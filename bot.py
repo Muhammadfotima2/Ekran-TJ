e4d8a4048d85eeb5e9827c285dd1d64fefd758ac
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-08-04 17:38:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
 
-# 🔐 Токен бота
+# 🔐 Токен Telegram-бота
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
 bot = telebot.TeleBot(TOKEN)
 
@@ -9,11 +9,14 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    catalog_btn = KeyboardButton("📦 Каталог", web_app=WebAppInfo(url="https://example.com"))
+    catalog_btn = KeyboardButton(
+        "📦 Каталог",
+        web_app=WebAppInfo(url="https://ekran-tj-production.up.railway.app/catalog.html")
+    )
     markup.add(catalog_btn)
     bot.send_message(message.chat.id, "Хуш омадед! Нажмите кнопку ниже:", reply_markup=markup)
 
-# ▶️ Запуск
+# ▶️ Запуск бота
 if __name__ == '__main__':
     print("✅ Бот запущен")
     bot.infinity_polling()
