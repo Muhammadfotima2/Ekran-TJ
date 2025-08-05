4265343ebe2f3c6722227ea627a4875d606f5801
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-08-05 07:13:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from threading import Thread
 import json
 import os
 
-TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
-ADMIN_CHAT_ID = 6172156061
+TOKEN = '8307281840:AAFUJ21F9-Ql7HPWkUXl8RhNonwRNTPYyJk'
+ADMIN_CHAT_ID = 6172156061  # Твой Telegram ID
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__, static_folder='public')
@@ -25,7 +25,6 @@ def write_json(file, data):
     with open(file, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
-# Flask маршруты
 @app.route('/')
 def index():
     return send_from_directory('public', 'catalog.html')
@@ -66,7 +65,6 @@ def admin_orders():
             html += '</div>'
     return html
 
-# Telegram-бот
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
@@ -79,7 +77,7 @@ def start_handler(message):
 
 @bot.message_handler(content_types=['web_app_data'])
 def handle_web_app_data(message):
-    print("Получены данные из WebApp:", message.web_app_data.data)  # Логирование
+    print("Получены данные из WebApp:", message.web_app_data.data)
     order_text = message.web_app_data.data
     user = message.from_user
     user_name = f"{user.first_name or ''} {user.last_name or ''}".strip() or "Клиент"
